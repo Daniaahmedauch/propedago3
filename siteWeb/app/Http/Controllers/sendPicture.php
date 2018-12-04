cbe46ec5dd5b92e8c00edf49735f4f486bd68845
dylangouzenne <dylan.gouzenne@gmail.com>
2018-12-04 14:35:33 +0100

    creation template et vue accueil + fonction show sendPicture.php

--- siteWeb/app/Http/Controllers/sendPicture.php
+++ siteWeb/app/Http/Controllers/sendPicture.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Picture;
 
 class sendPicture extends Controller
 {
-    //
+    public function show(){
+        $pictures = Picture::all();
+        return view('accueil', compact("accueil"));
+    }
 }
